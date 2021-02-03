37d8fc2a741eb84abce0488e060f21cffd14eca6
Ayako <usuajiayako@gmail.com>
2021-02-03 23:36:41 +0000

    changed APIFILE

--- src/APIFile/APIFile.js
+++ src/APIFile/APIFile.js
@@ -3,7 +3,7 @@ import { config } from '../config';
 export const fetchWeather = (searchParam) => {
   return (
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${searchParam}&units=metric&appid=${config.WEATHER_API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?q=${searchParam}&units=metric&appid=${config.WEATHER_API_KEY = "c12c2983febcb14d9c55561228fe703f"}`
     )
       .then((response) => {
         return response.json();
